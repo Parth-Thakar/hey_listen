958e841b681864535a8ca383110aee0d4667bc09
Parth Thakar <parththakar59@gmail.com>
2024-02-23 22:55:04 +0530

    Intitial project setup.

---
+++ app/src/main/java/com/example/heylisten/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.heylisten.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val onBackground = Color(0xFFd9b4e2)
+val background = Color(0xFFfffaf8)
+val redButton = Color(0xFFf0a6a6)
+val redButtonGradient2 = Color(0xFFce7e7f)
+val actionButtonColor = Color(0xFF806e97)
